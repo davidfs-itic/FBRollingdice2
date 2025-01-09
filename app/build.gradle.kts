4ed8da62d23c8a1861b15ef4d48929ae2d90b0fd
davidfs-itic <davidfs.docencia@gmail.com>
2025-01-09 15:46:54 +0100

    Afegim firebase. Seguim la guia a : https://firebase.google.com/docs/analytics/get-started?platform=android 1-Creem projecte a la web de firebase, a la console, (tal com diu a Before you begin) 2-Omplim la informació i descarreguem l'arxiu google-services.json 3-Afegim dependències als gradle (i al libs.versions.toml) 4-Creem l'objecte analytics a la MainActivity i fem logs.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -48,4 +49,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Import the BoM for the Firebase platform
+    implementation(platform( libs.firebase.bom))
+
+    // Add the dependency for the Analytics library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(libs.firebase.analytics)
 }
--- app/src/main/java/com/example/fbrollingdice2/MainActivity.kt
+++ app/src/main/java/com/example/fbrollingdice2/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fbrollingdice2.R
 import com.example.fbrollingdice2.databinding.ActivityMainBinding
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.logEvent
+import com.google.firebase.ktx.Firebase
+
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
 
         binding.txtDice1.text= getString(R.string.novaluestring)
         binding.txtDice2.text=getString(R.string.novaluestring)
@@ -23,7 +29,11 @@ class MainActivity : AppCompatActivity() {
             val dice1= Random.nextInt(1,6)
             val dice2= Random.nextInt(1,6)
             val sum =dice1+dice2
-
+            firebaseAnalytics.logEvent("RollDice",{
+                param("Dice1",dice1.toLong())
+                param("Dice2",dice2.toLong())
+                param("Double",(dice1==dice2).toString())
+            })
             binding.txtDice1.text=dice1.toString()
             binding.txtDice2.text=dice2.toString()
             binding.txtTotal.text=sum.toString()
