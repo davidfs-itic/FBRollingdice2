41d18d7ab682674a2aee0b531f03083b6ec89455
davidfs-itic <davidfs.docencia@gmail.com>
2025-01-09 13:48:20 +0100

    Commit inicial. Projecte funcional

---
+++ app/src/main/java/com/example/fbrollingdice2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.firebase_rollingdice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fbrollingdice2.R
+import com.example.fbrollingdice2.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.txtDice1.text= getString(R.string.novaluestring)
+        binding.txtDice2.text=getString(R.string.novaluestring)
+        binding.txtTotal.text=getString(R.string.novaluestring)
+        binding.btnRoll.setOnClickListener{
+            val dice1= Random.nextInt(1,6)
+            val dice2= Random.nextInt(1,6)
+            val sum =dice1+dice2
+
+            binding.txtDice1.text=dice1.toString()
+            binding.txtDice2.text=dice2.toString()
+            binding.txtTotal.text=sum.toString()
+        }
+    }
+
+}
